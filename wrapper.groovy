b9fe210afb0ced5d91c038b38c1679cb73653fa5
James McShane <jmcshan1@gmail.com>
2017-09-01 21:54:34 -0500

    Add elasticsearch call

--- wrapper.groovy
+++ wrapper.groovy
@@ -2,7 +2,9 @@ def callFunc(name, closure) {
     def start = System.currentTimeMillis();
     def e = null
     try {
-        closure()
+        stage(name) {
+            closure()
+        }
     } catch (Exception ex) {
         e = ex
         throw ex
@@ -12,7 +14,11 @@ def callFunc(name, closure) {
         if (e != null) {
             echo "${name} failed!"
         }
-        def response = httpRequest(url: 'http://google.com', timeout: 2000)
+        def stageMap = [jobName: env.JOB_NAME, jobNumber: env.BUILD_NUMBER,
+                        elapsedTime:${end - start}, success: (e == null)]
+        def response = httpRequest(url: 'http://elasticsearch-sonar-scan.192.168.0.118.nip.io', httpMode: 'POST',
+                                   requestBody: "${groovy.json.JsonOutput.toJson(stageMap)}")
+        println("Elasticsearch content: "+response.content)
     }
 }
 
